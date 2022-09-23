57e7dae5f1e12b9a8605068d7df891e8ec287ace
Scott Burton <burtons@byui.edu>
2022-09-23 15:38:54 -0600

    Moved the display into its own function.

--- sandbox/SandboxProject/Program.cs
+++ sandbox/SandboxProject/Program.cs
@@ -6,6 +6,11 @@ namespace SandboxProject
     {
         static void Main(string[] args)
         {
+            DisplayGreeting();
+        }
+
+        static void DisplayGreeting()
+        {
             Console.WriteLine("Welcome to the Tic Tac Toe Program!");
         }
     }
